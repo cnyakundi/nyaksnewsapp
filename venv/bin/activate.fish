dbbcac632c06c256d9bbd25e8b12b8b43be09caf
Cyprian  Nyakundi <cn@Cyprians-MacBook-Pro.local>
2020-10-19 15:36:44 +0300

    second commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cn/PycharmProjects/newsproject/venv"
+set -gx VIRTUAL_ENV "/Users/cn/Desktop/nyaksnewsapp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
